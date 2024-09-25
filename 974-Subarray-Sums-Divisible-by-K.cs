98fbd1e2465c8e92aeebc16cb334e8950d7612cb
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2024-09-25 16:44:00 +0300

    [974] [Time Beats: 79.73%] [Memory Beats: 5.41%] - LeetPush

---
+++ 974-Subarray-Sums-Divisible-by-K.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int SubarraysDivByK(int[] nums, int k)
+        {
+            int size = nums.Length;
+
+            for (int i = 1; i < size; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+            
+            for(int i = 0; i < size; i++){
+                nums[i] = nums[i] % k;
+                if (nums[i] < 0)
+                {
+                    nums[i] += k; 
+                }
+            }
+
+            int[] freq = new int[k];
+            Array.Fill(freq, 0);
+            int sol = 0;
+            freq[0] = 1; 
+            for (int i = 0; i < size; i++)
+            {
+                sol += freq[nums[i]];
+                freq[nums[i]]++; 
+            }
+
+            return sol; 
+  
+        }
+}
