af06b62289473f3db417ae627c0a1d809d5209e6
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2024-11-03 17:02:30 +0200

    [217] [Time Beats: 26.46%] [Memory Beats: 96.25%] - LeetPush

--- 217-Contains-Duplicate.cs
+++ 217-Contains-Duplicate.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        for(int i =0 ;i<nums.Length-1;i++){
-            for(int j =i+1;j<nums.Length;j++){
-                if(nums[i]==nums[j]){
-                    return true; 
-                }
+        Array.Sort(nums);
+        for(int i = 0 ;i<nums.Length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
             }
         }
-        return false; 
+        return false;
     }
 }
