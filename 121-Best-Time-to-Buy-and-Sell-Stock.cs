d539888ea1222943f47a1be6079f222a1871e86d
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2024-11-22 22:31:49 +0200

    [121] [Time Beats: 5.01%] [Memory Beats: 99.52%] - LeetPush

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0 ;
+
+        foreach(int price in prices){
+            minPrice = Math.Min(minPrice , price);
+            maxProfit = Math.Max(maxProfit , price - minPrice);
+        }
+
+        return maxProfit ; 
+    }
+}
