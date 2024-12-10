ec37e624de72bc1dd041e4f38e6d4a1469ef14fa
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2024-12-10 23:38:43 +0200

    [88] [Time Beats: 100.00%] [Memory Beats: 44.00%] - LeetPush

--- 88-Merge-Sorted-Array.cs
+++ 88-Merge-Sorted-Array.cs
@@ -1,9 +1,20 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-       for(int i = m ;i<m+n;i++){
-        nums1[i] = nums2[i-m];
-       }    
-       Array.Sort(nums1);      
+      int i = m-1 , j = n-1,k = n+m-1;
+      while(i>=0 && j>=0){
+          if(nums1[i]>nums2[j]){
+            nums1[k] = nums1[i--];
+          }else{
+            nums1[k] = nums2[j--];
+          }
+          k--; 
+      }
+
+      while(j>=0){
+        nums1[k--] = nums2[j--];
+      }
+
+
         
     }
 }
