2cfc77a6384d63d44e3a0d19b969f94990d202a6
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2024-12-11 18:50:20 +0200

    [125] [Time Beats: 68.03%] [Memory Beats: 58.34%] - LeetPush

--- 125-Valid-Palindrome.cs
+++ 125-Valid-Palindrome.cs
@@ -2,10 +2,9 @@ public class Solution {
     public bool IsPalindrome(string s) {
       StringBuilder sb = new StringBuilder();
       foreach(char c in s){
-        // I create c2 because i can not change c in foreach
-        char c2 = char.ToLower(c);
-        if((c2>=97 && c2<=122) || (c2>=48 && c2<=57) ){
-         sb.Append(c2);
+        // or char c2 = char.ToLower(c) 
+        if(char.IsLetterOrDigit(c)){
+         sb.Append(char.ToLower(c));
       }  
      }
 
