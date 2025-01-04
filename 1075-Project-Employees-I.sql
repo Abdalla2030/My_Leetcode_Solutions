1e38c38d46c75822e396435b483a06d5b6758532
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-01-04 17:05:02 +0200

    [1075] [Time Beats: 28.30%] - LeetPush

---
+++ 1075-Project-Employees-I.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+
+select Project.project_id, round(avg(experience_years),2) as  average_years
+from Project left join Employee
+on  Project.employee_id = Employee. employee_id
+group by Project.project_id
+
