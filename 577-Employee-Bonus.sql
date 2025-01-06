6aa9c74ec27b8d4b4cb09bbda5eae7f483a92853
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-01-06 13:17:27 +0200

    [577] [Time Beats: 5.12%] - LeetPush

--- 577-Employee-Bonus.sql
+++ 577-Employee-Bonus.sql
@@ -1,6 +1,9 @@
 /* Write your PL/SQL query statement below */
 
-select name , bonus 
-from Employee E left join Bonus B
-on E.empId = B.empId
-where bonus < 1000 or bonus is null ; 
+select name, bonus
+from Employee E
+left join Bonus B
+    on E.empId = B.empId
+where bonus < 1000 or bonus is null
+order by bonus nulls last;
+
