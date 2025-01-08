603f50b1eccfd775418ef89735f06f3789586404
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-01-08 13:57:30 +0200

    [1633] [Time Beats: 6.57%] - LeetPush

--- 1633-Percentage-of-Users-Attended-a-Contest.sql
+++ 1633-Percentage-of-Users-Attended-a-Contest.sql
@@ -1,9 +1,5 @@
-/* Write your PL/SQL query statement below */
-
-SELECT r.contest_id,
-       ROUND(COUNT(DISTINCT r.user_id) * 100 / (SELECT COUNT(user_id) FROM Users), 2) AS percentage
-FROM Register r
-GROUP BY r.contest_id
-ORDER BY percentage DESC, r.contest_id ASC;
-
-
+select  R.contest_id ,round((Count(R.user_id) / (select count(*) from Users))*100,2) as percentage
+from Register R inner join Users U
+on R.user_id = U.user_id
+group by R.contest_id;
+order by  percentage desc ,  R.contest_id; 
