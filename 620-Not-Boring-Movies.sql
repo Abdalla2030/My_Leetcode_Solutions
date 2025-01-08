5f82858eeefec548c0bbff7dcaf1fff4e2ecfa2a
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-01-08 12:08:28 +0200

    [620] [Time Beats: 19.89%] - LeetPush

--- 620-Not-Boring-Movies.sql
+++ 620-Not-Boring-Movies.sql
@@ -1,6 +1,6 @@
 /* Write your PL/SQL query statement below */
 
-select *
-from Cinema
-where mod(id,2) = 1 and description <> 'boring'
+select id, movie, description, rating
+from Cinema 
+where mod(id,2)=1 and description <> 'boring'
 order by rating desc;
