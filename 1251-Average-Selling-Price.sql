c7194dcf4eda394aea43b2a09d0672941faa190b
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-01-08 12:28:12 +0200

    [1251] [Time Beats: 17.20%] - LeetPush

--- 1251-Average-Selling-Price.sql
+++ 1251-Average-Selling-Price.sql
@@ -1,13 +1,7 @@
 /* Write your PL/SQL query statement below */
 
-SELECT 
-    prices.product_id, 
-    COALESCE(ROUND(SUM(price * units) / SUM(units), 2), 0) AS average_price
-FROM 
-    prices 
-LEFT JOIN 
-    UnitsSold 
-ON 
-    prices.product_id = UnitsSold.product_id 
-    AND purchase_date BETWEEN prices.start_date AND prices.end_date
-GROUP BY prices.product_id;
+select P.product_id , nvl(round(sum(U.units*P.price)/sum(U.units),2),0)as average_price
+from UnitsSold U right join Prices P
+on  U.product_id = P.product_id 
+and U.purchase_date between P.start_date AND P.end_date
+group by P.product_id ;
