50554fcc56242a486a1f2f1d5621d5f31ed2a044
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-02-08 21:10:02 +0200

    [1978] [Time Beats: 65.58%] - LeetPush

---
+++ 1978-Employees-Whose-Manager-Left-the-Company.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+
+select employee_id
+from Employees 
+where salary < 30000
+and manager_id not in (select distinct employee_id
+                        from Employees)
+order by employee_id;
