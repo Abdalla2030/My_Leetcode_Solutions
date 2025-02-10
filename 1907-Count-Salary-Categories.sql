a33cfc99c737545ae534c85b92ce8b380a3007a5
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-02-10 14:30:16 +0200

    [1907] [Time Beats: 15.60%] - LeetPush

---
+++ 1907-Count-Salary-Categories.sql
@@ -0,0 +1,16 @@
+select 'Low Salary' category, count(*) accounts_count
+from Accounts 
+where income < 20000
+
+union all
+
+select 'Average Salary' category, count(*) accounts_count
+from Accounts 
+where income >= 20000 and income <= 50000
+
+union all 
+
+select 'High Salary' category, count(*) accounts_count
+from Accounts 
+where income > 50000
+
