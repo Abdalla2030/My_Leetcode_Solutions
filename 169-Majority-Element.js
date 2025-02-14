74619c7f7b8381948df07757a52d79a2953311f2
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-02-15 00:01:38 +0200

    [169] [Time Beats: 100.00%] [Memory Beats: 90.54%] - LeetPush

--- 169-Majority-Element.js
+++ 169-Majority-Element.js
@@ -3,21 +3,16 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let dic = new Map();
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (dic.has(nums[i])) {
-            dic.set(nums[i], dic.get(nums[i]) + 1);
-        } else {
-            dic.set(nums[i], 1);
+    let res = mag = 0 ;
+    for(let i = 0 ;i<nums.length;i++){
+        if(mag==0){
+            res = nums[i];
+        }
+        if(nums[i]==res){
+            mag++;
+        }else{
+            mag--;
         }
     }
-
-    for (let [key, value] of dic) {
-        if (value > nums.length / 2) {
-            return key;
-        }
-    }
-
-    return -1;
-};
+    return res ; 
+}
