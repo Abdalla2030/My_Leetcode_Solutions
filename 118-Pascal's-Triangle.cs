794049a0fd1cca7d1d6e32bb1cd490828188390f
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-02-14 23:31:00 +0200

    [118] [Time Beats: 3.12%] [Memory Beats: 98.13%] - LeetPush

--- 118-Pascal's-Triangle.cs
+++ 118-Pascal's-Triangle.cs
@@ -1,22 +1,19 @@
 public class Solution {
     public IList<IList<int>> Generate(int numRows) {
-        IList<IList<int>> triangle = new List<IList<int>>();
-
-        for (int i = 0; i < numRows; i++) {
-            IList<int> r = new List<int>(i + 1);
-            r.Add(1);
-
-            for (int j = 1; j < i; j++) {
-                r.Add(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+    IList<IList<int>> triangle = new List<IList<int>>();
+    
+    for (int i = 0; i < numRows; i++) {
+        List<int> currentRow = new List<int>();
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) {
+                currentRow.Add(1);
+            } else {
+                currentRow.Add(triangle[i - 1][j - 1] + triangle[i - 1][j]);
             }
-
-            if (i > 0) {
-                r.Add(1);
-            }
-
-            triangle.Add(r);
         }
-
-        return triangle;
+        triangle.Add(currentRow);
     }
+    
+    return triangle;
+}
 }
