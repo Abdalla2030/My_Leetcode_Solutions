c637024cad9f9d1645408fe492a0311a3e548927
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-02-16 15:51:14 +0200

    [1321] [Time Beats: 45.28%] - LeetPush

--- 1321-Restaurant-Growth.sql
+++ 1321-Restaurant-Growth.sql
@@ -1,19 +1,19 @@
 /* Write your PL/SQL query statement below */
 
-select distinct to_char(visited_on,'yyyy-mm-dd') visited_on,
-                (select sum(amount) from Customer  
-                   where visited_on between c.visited_on-6 and  c.visited_on
-                   ) amount ,
-                   (select round(sum(amount)/7,2)
-                    from Customer
-                    where visited_on between c.visited_on-6 and  c.visited_on)  average_amount   
-from Customer c 
-where visited_on in (
-                    select c2.visited_on
-                    from Customer c1, Customer c2
-                    where c2.visited_on >= c1.visited_on+6
-                    )
-order by  visited_on;
+select distinct 
+    to_char(c.visited_on,'yyyy-mm-dd') visited_on,
+    (select sum(amount) 
+     from Customer  
+     where visited_on between c.visited_on-6 and c.visited_on
+    ) amount,
+    (select round(sum(amount)/7,2)
+     from Customer
+     where visited_on between c.visited_on-6 and c.visited_on
+    ) average_amount   
+from Customer c, Customer c2 
+where c.visited_on >= c2.visited_on + 6
+order by visited_on;
+
 
 
 
