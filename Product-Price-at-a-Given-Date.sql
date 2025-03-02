e8539dd86aed5e0dd0b1dcfe4521fea9cd031dc4
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-03-02 11:57:30 +0200

    [1164] [Time Beats: 98.31%] - LeetPush

--- Product-Price-at-a-Given-Date.sql
+++ Product-Price-at-a-Given-Date.sql
@@ -7,8 +7,9 @@ where (product_id, change_date) in
                                 from Products
                                 where change_date <= '2019-08-16'
                                 group by product_id)
-                                union
-                                select product_id, 10 as price
-                                from Products
-                                group by product_id
-                                having min(change_date) > '2019-08-16';
+union all 
+select product_id, 10 as price
+from Products
+group by product_id
+having min(change_date) > '2019-08-16';
+
