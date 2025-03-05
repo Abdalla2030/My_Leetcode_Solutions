a134d54818b98423942dc4ab59379fe7dd1ad2f1
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-03-05 23:22:54 +0200

    [2807] [Time Beats: 95.00%] [Memory Beats: 80.00%] - LeetPush

---
+++ Insert-Greatest-Common-Divisors-in-Linked-List.cs
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    private int GCD(int a, int b) {
+        return (b == 0) ? a : GCD(b, a % b);
+    }
+    public ListNode InsertGreatestCommonDivisors(ListNode head) {
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;  
+
+        while (curr != null) {
+            int gcdValue = GCD(prev.val, curr.val);
+
+            ListNode newNode = new ListNode(gcdValue);
+            prev.next = newNode;
+            newNode.next = curr;
+
+            prev = curr;
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
