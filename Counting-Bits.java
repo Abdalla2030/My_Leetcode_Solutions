b7b26d6b94f06e9aad9a432af98be3af32e5ae36
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-10-11 01:41:29 +0300

    [338] [Time Beats: 94.14%] [Memory Beats: 94.34%] - LeetPush

--- Counting-Bits.java
+++ Counting-Bits.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] countBits(int n) {
         int[] res = new int[n+1];
-        for(int i = 0;i<=n;i++){
-            int cnt = 0 ; 
-            int x = i ; 
-            while(x>0){
-                cnt +=  x & 1 ; 
-                x = x >> 1 ; 
-            }
-            res[i] = cnt ; 
+        res[0] = 0 ;
+        if(n>0){
+            res[1] = 1 ;
+            for(int i = 2;i<=n;i++){
+                res[i] = res[i>>1] + (i & 1) ;
+            } 
         }
-
         return res ; 
     }
 }
