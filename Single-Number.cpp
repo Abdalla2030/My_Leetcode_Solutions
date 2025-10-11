9367dfea936cac3d40952270cbd4bdeb12c58eb9
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-10-11 23:23:37 +0300

    [136] [Time Beats: 10.34%] [Memory Beats: 8.90%] - LeetPush

---
+++ Single-Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+         for (int n : nums) {
+            mp[n]++;
+        }
+        
+        for (auto& p : mp) {
+            if (p.second == 1)
+                return p.first;
+        }
+
+        return -1;
+
+    }
+};
