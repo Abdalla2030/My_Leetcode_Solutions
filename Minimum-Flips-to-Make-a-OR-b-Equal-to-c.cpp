1819b0ba81f6c1ae28dfc9ddb3792880a1568f47
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2025-10-17 19:32:24 +0300

    [1318] [Time Beats: 52.32%] [Memory Beats: 99.10%] - LeetPush

---
+++ Minimum-Flips-to-Make-a-OR-b-Equal-to-c.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int flips = 0 ;
+        while (a > 0 || b > 0 || c > 0){
+            if(c&1){
+                if(((a&1)==0) && ((b&1)==0)){
+                    flips += 1 ;
+                }
+            }else{
+                flips += (a&1) + (b&1);
+            }
+            a = a >> 1 ;
+            b = b >> 1 ;
+            c = c >> 1 ; 
+        }
+        return flips; 
+    }
+};
